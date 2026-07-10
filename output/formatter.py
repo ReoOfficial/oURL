bdd8768a8dec04ba1210722f84ca51ac802f5e37
Your Name <your-email@example.com>
2026-07-10 12:10:43 +0300

    -v modifications

--- output/formatter.py
+++ output/formatter.py
@@ -1,15 +1,34 @@
+from urllib.parse import urlparse
+
+
 def format_response(request, response):
     output = ""
 
     if request.verbose:
-        output += f"> {request.method} {request.url}\n"
+
+        parsed = urlparse(request.url)
+        path = parsed.path or "/"
+
+        output += f"> {request.method} {path} HTTP/1.1\n"
+
+        output += f"> Host: {parsed.netloc}\n"
+
+        if request.user_agent:
+            output += f"> User-Agent: {request.user_agent}\n"
+        else:
+            output += "> User-Agent: MyCurl/1.0\n"
+
+        output += "> Accpet: */*\n"
 
         for key, value in request.headers.items():
             output += f"> {key}: {value}\n"
 
         output += "\n"
 
-        output += f"< HTTP {response.get_status_code()} {response.get_reason()}\n"
+        output += (
+            f"< HTTP {response.get_status_code()} "
+            f"{response.get_reason()}\n"
+        )
 
 
         for key, value in response.get_headers().items():
